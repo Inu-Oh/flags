56811bdff70745d54038c8c4fb4039c391f4e70d
Inu Oh <ethanol-mace-swan@duck.com>
2026-02-26 21:00:31 -0500

    populate CSV view get method with test of CSV data

--- flags/views.py
+++ flags/views.py
@@ -1,13 +1,14 @@
-from django.contrib.auth.decorators import login_required
+from csv import reader
+
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView
 
 from .forms import PopulateDbForm
+from .models import Country
 
 
 # Create your views here.
-@login_required
 def index(request):
     return render(request, 'index.html')
 
@@ -25,12 +26,48 @@ class PopulateDbView(PermissionRequiredMixin, CreateView):
         if not request.user.is_superuser:
             return redirect('index')
         
-        #TODO : Test data.csv and provide report
-        #TODO : if csv invalid show error and hide form
+        try:
+            old_pk_list = Country.objects.values_list('id', flat=True)
+        except:
+            old_pk_list = []
 
+        try:
+            with open('data.csv') as csv:
+                new = 0
+                updated_pk_list = []
+                data_reader = reader(csv)
+                for row in data_reader:
+                    try: 
+                        _, _, _ = row[0], row[1], row[2]
+                    except IndexError:
+                        raise ValueError
+                    try:
+                        pk_str = row[3]
+                    except:
+                        pk_str = False
+                    if pk_str:
+                        try:
+                            pk = int(pk)
+                            updated_pk_list.append(pk)
+                        except:
+                            TypeError
+                    else:
+                        new += 1
+        except (ValueError, TypeError):
+            msg = "Error: Review data.csv and fix errors then refresh this page."
+            return render(request, self.template_name, { 'message': msg })
+        
+        edits, deletions = 0, 0
+        for int in updated_pk_list:
+            if int in old_pk_list:
+                edits += 1
+            else:
+                deletions 
+
+        msg = f'New entries: {new}\nEdited entries: {edits}\nDeleted entries: {deletions}'   
         form = PopulateDbForm()
+        context = { 'form': form, 'message': msg }
 
-        context = { 'form': form }
         return render(request, self.template_name, context)
     
     def post(self, request):
@@ -41,12 +78,11 @@ class PopulateDbView(PermissionRequiredMixin, CreateView):
 
         #TODO : are the next 3 lines necessary given the form is just a submit button
         if not form.is_valid():
-            context = { 'form': form }
-            return render(request, self.template_name, context)
+            return render(request, self.template_name, { 'form': form }
+)
         
         #TODO : clear any data removed from CSV - i.e. primary key in DB not found in CSV
         #TODO : process and puplate database based on CSV data changes / add new fields
         
         msg = 'Data successfully posted'
-        context = { 'message': msg }
-        return render(request, self.template_name, context)
+        return render(request, self.template_name, { 'message': msg })
