4b958762a34cab96bd241d73959bd7589ae633b8
Inu Oh <ethanol-mace-swan@duck.com>
2026-02-27 19:09:00 -0500

    DB populate view - verify CSV and populate DB - step 2

--- flags/views.py
+++ flags/views.py
@@ -1,6 +1,8 @@
 from csv import reader
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
 from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView
 
@@ -36,13 +38,14 @@ class PopulateDbView(PermissionRequiredMixin, CreateView):
                 new = 0
                 updated_pk_list = []
                 data_reader = reader(csv)
+                next(data_reader)
                 for row in data_reader:
                     try: 
                         _, _, _ = row[0], row[1], row[2]
                     except IndexError:
                         raise ValueError
                     try:
-                        pk_str = row[3]
+                        pk_str = row[4]
                     except:
                         pk_str = False
                     if pk_str:
@@ -52,26 +55,35 @@ class PopulateDbView(PermissionRequiredMixin, CreateView):
                         except:
                             TypeError
                     else:
-                        new += 1
+                        new += 1          
         except (ValueError, TypeError):
             msg = "Error: Review data.csv and fix errors then refresh this page."
             return render(request, self.template_name, { 'message': msg })
         
-        edits, deletions = 0, 0
-        for int in updated_pk_list:
-            if int in old_pk_list:
+        for pk in updated_pk_list:
+            if pk not in old_pk_list:
+                msg = f"Error: primary key {pk} does not exist in database."
+                msg += "<br>Correct CSV then refresh the page"
+                return render(request, self.template_name, { 'message': msg })
+
+        edits, deletions = 0, []
+        for pk in updated_pk_list:
+            if pk in old_pk_list:
                 edits += 1
             else:
-                deletions 
+                deletions.append(pk)
 
         if new == 0 and edits == 0 and deletions == 0:
             msg = 'Nothing to edit'
             return render(request, self.template_name, { 'message': msg })
         
-        msg = f'New entries: {new}<br>Edited entries: {edits}<br>Deleted entries: {deletions}'   
+        msg = f"New entries: {new}<br>Edited entries: {edits}"
+        msg += f"<br>Deleted entries: {len(deletions)}" 
         form = PopulateDbForm()
         context = { 'form': form, 'message': msg }
 
+        if deletions:
+            request.session['deletions'] = deletions
         return render(request, self.template_name, context)
     
     def post(self, request):
@@ -80,13 +92,56 @@ class PopulateDbView(PermissionRequiredMixin, CreateView):
         
         form = PopulateDbForm(request.POST)
 
-        #TODO : are the next 3 lines necessary given the form is just a submit button
         if not form.is_valid():
-            return render(request, self.template_name, { 'form': form }
-)
+            return render(request, self.template_name, { 'form': form })
+
+        with open('data.csv') as csv:
+            data_reader = reader(csv)
+            next(data_reader)
+            for row in data_reader:
+                try:
+                    pk = int(row[4])
+                except:
+                    pk = False
+                # pk value was verified in get method / Edit existing country objects
+                if pk:
+                    country = Country.objects.get(id=pk)
+                    country.country = row[0]
+                    country.capital = row[1]
+                    country.country_code = row[2]
+                    try:
+                        country.hint = row[3]
+                    except IndexError:
+                        pass
+                    country.save()
+                    continue
+                # Create new country objects
+                try:
+                    hint = row[3]
+                    Country.objects.create(
+                        country=row[0], 
+                        capital=row[1], 
+                        country_code=row[2],
+                        hint=hint
+                    )
+                except IntegrityError:
+                    pass
+                except IndexError:
+                    Country.objects.create(
+                        country=row[0], 
+                        capital=row[1], 
+                        country_code=row[2]
+                    )
+
+        # Delete items from DB that are not in the updated CSV
+        deletions = request.session.get['deletions']
+        del request.session['deletions']
+
+        for pk in deletions:
+            country = Country.objects.get(id=pk)
+            country.delete()
         
-        #TODO : clear any data removed from CSV - i.e. primary key in DB not found in CSV
-        #TODO : process and puplate database based on CSV data changes / add new fields
-        
+        #TODO : create updated CSV of updated DB
+
         msg = 'Data successfully posted'
         return render(request, self.template_name, { 'message': msg })
